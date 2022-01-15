a2374b4af72b29327179700891b9bdeff406e0a4
itzhouq <shangguan10>
2022-01-15 10:44:22 +0800

    .feat (.location) :<引入支付参数|引入商户私钥|获取验签器>

--- src/main/java/cn/itzhouq/payment/weixin/config/WxPayConfig.java
+++ src/main/java/cn/itzhouq/payment/weixin/config/WxPayConfig.java
@@ -1,14 +1,30 @@
 package cn.itzhouq.payment.weixin.config;
 
+import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
+import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
+import com.wechat.pay.contrib.apache.httpclient.auth.ScheduledUpdateCertificatesVerifier;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Validator;
+import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import lombok.Data;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
+
 @Configuration
 @PropertySource("classpath:wxpay.properties") //读取配置文件
-@ConfigurationProperties(prefix="wxpay") //读取wxpay节点
+@ConfigurationProperties(prefix = "wxpay") //读取wxpay节点
 @Data //使用set方法将wxpay节点中的值填充到当前类的属性中
+@Slf4j
 public class WxPayConfig {
 
     // 商户号
@@ -31,5 +47,70 @@ public class WxPayConfig {
 
     // 接收结果通知地址
     private String notifyDomain;
+
+
+    /**
+     * @param filename 文件名
+     * @return {@link java.security.PrivateKey}
+     * @Description 获取商户私钥
+     * @author itzhouq
+     * @Date 2022/1/14 07:27
+     */
+    private PrivateKey getPrivateKey(String filename) {
+        try {
+            return PemUtil.loadPrivateKey(
+                    new FileInputStream(filename));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("私钥文件不存在", e);
+        }
+    }
+
+
+    /**
+     * @return {@link ScheduledUpdateCertificatesVerifier}
+     * @Description 获取签名验证器
+     * @author itzhouq
+     * @Date 2022/1/14 07:47
+     */
+    @Bean
+    public ScheduledUpdateCertificatesVerifier getVerifier() {
+        log.info("获取签名验证器");
+        //获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+
+        //私钥签名对象
+        PrivateKeySigner privateKeySigner = new PrivateKeySigner(mchSerialNo, privateKey);
+
+        //身份认证对象
+        WechatPay2Credentials wechatPay2Credentials = new WechatPay2Credentials(mchId, privateKeySigner);
+
+        // 使用定时更新的签名验证器，不需要传入证书
+
+        return new ScheduledUpdateCertificatesVerifier(
+                wechatPay2Credentials,
+                apiV3Key.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * @param verifier 签名验证器
+     * @return {@link org.apache.http.impl.client.CloseableHttpClient}
+     * @Description 获取http请求对象
+     * @author itzhouq
+     * @Date 2022/1/14 07:46
+     */
+    @Bean(name = "wxPayClient")
+    public CloseableHttpClient getWxPayClient(ScheduledUpdateCertificatesVerifier verifier) {
+        log.info("获取httpClient");
+
+        //获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
+                .withMerchant(mchId, mchSerialNo, privateKey)
+                .withValidator(new WechatPay2Validator(verifier));
+        // ... 接下来，你仍然可以通过builder设置各种参数，来配置你的HttpClient
+        // 通过WechatPayHttpClientBuilder构造的HttpClient，会自动的处理签名和验签，并进行证书自动更新
+        return builder.build();
+    }
 
 }
--- src/test/java/cn/itzhouq/payment/weixin/PaymentWeixinApplicationTests.java
+++ src/test/java/cn/itzhouq/payment/weixin/PaymentWeixinApplicationTests.java
@@ -1,13 +1,33 @@
 package cn.itzhouq.payment.weixin;
 
+import cn.itzhouq.payment.weixin.config.WxPayConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.security.PrivateKey;
 
 @SpringBootTest
 class PaymentWeixinApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Resource
+    private WxPayConfig wxPayConfig;
+
+//    /**
+//     * @Description 测试获取商户私钥
+//     * @author itzhouq
+//     * @Date 2022/1/14 07:33
+//     */
+//    @Test
+//    void testGetPrivateKey() {
+//
+//        // 获取私钥路径
+//        String privateKeyPath = wxPayConfig.getPrivateKeyPath();
+//
+//        // 获取私钥
+//        PrivateKey privateKey = wxPayConfig.getPrivateKey(privateKeyPath);
+//        System.out.println(privateKey);
+//
+//    }
 
 }
