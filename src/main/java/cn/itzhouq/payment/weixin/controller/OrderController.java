902b5d090ca21a31506fd97e086abe9858a66f5c
itzhouq <shangguan10>
2022-01-18 07:21:17 +0800

    .feat (.location) :<商户定时查询本地订单>

--- src/main/java/cn/itzhouq/payment/weixin/controller/OrderController.java
+++ src/main/java/cn/itzhouq/payment/weixin/controller/OrderController.java
@@ -1,17 +1,16 @@
 package cn.itzhouq.payment.weixin.controller;
 
 import cn.itzhouq.payment.weixin.entity.OrderInfo;
+import cn.itzhouq.payment.weixin.enums.OrderStatus;
 import cn.itzhouq.payment.weixin.service.OrderInfoService;
 import cn.itzhouq.payment.weixin.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 订单管理Controller
@@ -33,5 +32,15 @@ public class OrderController {
     public R list() {
         List<OrderInfo> list = orderInfoService.listOrderByCreateTimeDesc();
         return R.ok().data("list", list);
+    }
+
+    @ApiOperation("查询本地订单状态")
+    @GetMapping("/query-order-status/{orderNo}")
+    public R queryOrderStatus(@PathVariable String orderNo) {
+        String orderStatus = orderInfoService.getOrderStatus(orderNo);
+        if (Objects.equals(OrderStatus.SUCCESS.getType(), orderStatus)) {
+            return R.ok().setMessage("支付成功");
+        }
+        return R.ok().setCode(101).setMessage("支付中。。。。");
     }
 }
